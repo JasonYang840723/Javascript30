9f213c802b073f25fdf27bfb65413fce847d0867
unknown <jasons0900107315@gmail.com>
2021-03-19 23:19:48 +0800

    加入hover效果與click事件

---
+++ 01_JavaScript Drum Kit/script.js
@@ -0,0 +1,20 @@
+function playSound(e) {
+  let keyNo = e.keyCode || this.getAttribute("data-key");
+  const audio = document.querySelector(`audio[data-key="${keyNo}"]`);
+  const key = document.querySelector(`.key[data-key="${keyNo}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.toggle("playing");
+}
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+  key.addEventListener("click", playSound);
+});
+window.addEventListener("keydown", playSound);
