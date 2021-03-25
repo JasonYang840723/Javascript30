58a3aeb3f3e8d95842f31e6b724108e471074788
unknown <jasons0900107315@gmail.com>
2021-03-25 17:22:53 +0800

    Initial commit

--- 01_JavaScript Drum Kit/script.js
+++ 01_JavaScript Drum Kit/script.js
@@ -1,20 +1,20 @@
 function playSound(e) {
-  let keyNo = e.keyCode || this.getAttribute("data-key");
+  let keyNo = e.keyCode || this.getAttribute('data-key');
   const audio = document.querySelector(`audio[data-key="${keyNo}"]`);
   const key = document.querySelector(`.key[data-key="${keyNo}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-  key.classList.toggle("playing");
+  key.classList.toggle('playing');
 }
 function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  this.classList.remove("playing");
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
 }
 
-const keys = Array.from(document.querySelectorAll(".key"));
+const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach((key) => {
-  key.addEventListener("transitionend", removeTransition);
-  key.addEventListener("click", playSound);
+  key.addEventListener('transitionend', removeTransition);
+  key.addEventListener('click', playSound);
 });
-window.addEventListener("keydown", playSound);
+window.addEventListener('keydown', playSound);
